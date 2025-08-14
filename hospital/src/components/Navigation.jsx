0b3197c4d733e3026b2cd9a3b06adb82c08bf3be
pradeepkumar673 <pradeepcastleofficial@gmail.comexit>
2025-08-14 14:03:34 +0530

    2nd commit

--- hospital/src/components/Navigation.jsx
+++ hospital/src/components/Navigation.jsx
@@ -7,7 +7,6 @@ function Navigation() {
         <div className="w-full flex justify-between items-center p-6 bg-rose-300 px-10 relative font-cursive">
             <h1 className="text-xl font-bold font-sans">Central Los Santosh Medical Center</h1>
             
-            {/* Hamburger Icon */}
             <button 
                 className="lg:hidden focus:outline-none"
                 onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -23,28 +22,26 @@ function Navigation() {
                 </svg>
             </button>
 
-            {/* Desktop Navigation */}
             <ul className="hidden lg:flex gap-4 font-sans font-semibold">
-                <li><a className="hover:underline font-semibold" href="#">Home</a></li>
-                <li><a className="hover:underline font-semibold" href="#">About</a></li>
-                <li><a className="hover:underline font-semibold" href="#">Contact</a></li>
-                <li><a className="hover:underline font-semibold" href="#">Login</a></li>
-                <li><a className="hover:underline font-semibold" href="#">Register</a></li>
+                <li><a className="hover:underline font-semibold" href="https://shattereddisk.github.io/rickroll/rickroll.mp4">Home</a></li>
+                <li><a className="hover:underline font-semibold" href="https://shattereddisk.github.io/rickroll/rickroll.mp4">About</a></li>
+                <li><a className="hover:underline font-semibold" href="https://shattereddisk.github.io/rickroll/rickroll.mp4">Contact</a></li>
+                <li><a className="hover:underline font-semibold" href="https://shattereddisk.github.io/rickroll/rickroll.mp4">Login</a></li>
+                <li><a className="hover:underline font-semibold" href="https://shattereddisk.github.io/rickroll/rickroll.mp4">Register</a></li>
             </ul>
 
-            {/* Mobile Menu */}
             <div className={`lg:hidden fixed inset-0 z-40 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-                {/* Overlay */}
+             
                 <div 
                     className="fixed inset-0 bg-black bg-opacity-50"
                     onClick={() => setIsMenuOpen(false)}
                 />
                 
-                {/* Sidebar */}
+              
                 <div 
                     className={`fixed right-0 top-0 h-full w-64 bg-rose-300 z-50 p-6 shadow-lg transition-all duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}
                 >
-                    {/* Close Button - now properly visible */}
+                    
                     <button 
                         className="absolute right-6 top-6 focus:outline-none text-gray-800 hover:text-black transition-colors"
                         onClick={() => setIsMenuOpen(false)}
@@ -60,13 +57,13 @@ function Navigation() {
                         </svg>
                     </button>
 
-                    {/* Mobile Menu Items */}
+                    
                     <ul className="flex flex-col gap-6 font-sans font-semibold mt-10">
-                        <li><a className="hover:underline font-semibold block" href="#" onClick={() => setIsMenuOpen(false)}>Home</a></li>
-                        <li><a className="hover:underline font-semibold block" href="#" onClick={() => setIsMenuOpen(false)}>About</a></li>
-                        <li><a className="hover:underline font-semibold block" href="#" onClick={() => setIsMenuOpen(false)}>Contact</a></li>
-                        <li><a className="hover:underline font-semibold block" href="#" onClick={() => setIsMenuOpen(false)}>Login</a></li>
-                        <li><a className="hover:underline font-semibold block" href="#" onClick={() => setIsMenuOpen(false)}>Register</a></li>
+                        <li><a className="hover:underline font-semibold block" href="https://shattereddisk.github.io/rickroll/rickroll.mp4" onClick={() => setIsMenuOpen(false)}>Home</a></li>
+                        <li><a className="hover:underline font-semibold block" href="https://shattereddisk.github.io/rickroll/rickroll.mp4" onClick={() => setIsMenuOpen(false)}>About</a></li>
+                        <li><a className="hover:underline font-semibold block" href="https://shattereddisk.github.io/rickroll/rickroll.mp4" onClick={() => setIsMenuOpen(false)}>Contact</a></li>
+                        <li><a className="hover:underline font-semibold block" href="https://shattereddisk.github.io/rickroll/rickroll.mp4" onClick={() => setIsMenuOpen(false)}>Login</a></li>
+                        <li><a className="hover:underline font-semibold block" href="https://shattereddisk.github.io/rickroll/rickroll.mp4" onClick={() => setIsMenuOpen(false)}>Register</a></li>
                     </ul>
                 </div>
             </div>
